f6bf560c156ee9a10e723aa3ce0b78fb0669d92b
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-07-31 17:38:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,16 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+    #check for player 1 win
+    if check_win_combination?(board, 'X', win_combo)
+      return win_combo
+      #check for player 2 win
+    elsif check_win_combination?(board, 'O', win_combo)
+      return win_combo
+    else
+      return false
+    end
+  end
+end
